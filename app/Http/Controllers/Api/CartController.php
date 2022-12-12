4f36e7dc5f4f3a8f05581f1ebf35bf3f38c596f3
Hein Ko Zin <heinkozin4@gmail.com>
2022-12-12 10:08:41 +0630

    cart qty same item increase qty added

--- app/Http/Controllers/Api/CartController.php
+++ app/Http/Controllers/Api/CartController.php
@@ -13,7 +13,7 @@ class CartController extends BaseController
     public function getByUserIdCart($id)
     {
         try{
-            $carts = new CartCollection(Cart::where('user_id', $id)->paginate(10));
+            $carts = new CartCollection(Cart::where('user_id', $id)->where('is_active', true)->paginate(10));
             // $carts = json_decode(json_encode($carts));
             return $this->sendResponse($carts,"Cart data getting successfully!");
 
@@ -25,12 +25,26 @@ class CartController extends BaseController
     public function createCart(Request $request)
     {
         try{
-            $order = Cart::create([
-                'user_id' => $request->user_id,
-                'product_id' => $request->product_id,
-                'status' => $request->status,
-            ]);
-            return $this->sendResponse($order,"Cart Added successfully!.");
+            $old_cartId = Cart::where('user_id', $request->user_id)->where('product_id', $request->product_id)->value('id');
+            if($old_cartId){
+                $old_qty = Cart::where('user_id', $request->user_id)->where('product_id', $request->product_id)->value('qty');
+                $new_qty = $old_qty + 1;
+                Cart::where('id', $old_cartId)->update([
+                    'user_id' => $request->user_id,
+                    'product_id' => $request->product_id,
+                    'status' => $request->status,
+                    'qty' => $new_qty
+                ]);
+            }else{
+                $qty = 1;
+                Cart::create([
+                    'user_id' => $request->user_id,
+                    'product_id' => $request->product_id,
+                    'status' => $request->status,
+                    'qty' => $qty
+                ]);
+            }
+            return $this->sendMessageResponse("Cart Added successfully!.");
         }catch(Exception $e){
             return $this->sendError($e->getMessage());
         }
--- app/Models/Cart.php
+++ app/Models/Cart.php
@@ -15,6 +15,7 @@ class Cart extends Model
         'status',
         'is_preorder',
         'is_active',
+        'qty'
     ];
 
     public function user()
