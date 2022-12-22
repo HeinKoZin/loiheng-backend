82bf9d57993092a13ac0e6d7413f86dfbb15194f
Hein Ko Zin <heinkozin4@gmail.com>
2022-12-22 17:02:59 +0630

    cart api changed

--- app/Http/Controllers/Api/CartController.php
+++ app/Http/Controllers/Api/CartController.php
@@ -8,6 +8,7 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\CartCollection;
 use App\Http\Resources\CartResource;
+use App\Models\CartItem;
 
 class CartController extends BaseController
 {
@@ -29,28 +30,65 @@ class CartController extends BaseController
     {
         try{
             $user = auth('sanctum')->user();
-            $old_cartId = Cart::where('is_active', true)->where('user_id', $user->id)->where('product_id', $request->product_id)->value('id');
-            if($old_cartId){
-                $old_qty = Cart::where('is_active', true)->where('user_id', $user->id)->where('product_id', $request->product_id)->value('qty');
-                $new_qty = $old_qty + 1;
-                 Cart::where('id', $old_cartId)->update([
-                    'user_id' => $user->id,
-                    'product_id' => $request->product_id,
-                    'status' => $request->status,
-                    'qty' => $new_qty
-                ]);
-                $cart = CartResource::collection(Cart::where('id', $old_cartId)->get());
-            }else{
-                $qty = 1;
+            $cart = Cart::where('user_id', $user->id)->where('is_active', true)->first();
+            // return $cart->id;
+            if($cart->is_active != true){
                 $cart = Cart::create([
-                    'user_id' => $user->id,
+                    'user_id' => $user->id
+                ]);
+            }
+            // return $cart;
+            $old_cart_id = CartItem::where('cart_id', $cart->id)->where('product_id', $request->product_id)->value('id');
+            $old_qty = CartItem::where('cart_id', $cart->id)->where('product_id', $request->product_id)->value('qty');
+            if($old_cart_id){
+                if(is_null($request->qty)){
+                    $old_qty =  $old_qty + 1;
+                }else{
+                    $old_qty = $request->qty;
+                }
+                $oldCartData = CartItem::where('id', $old_cart_id)->update([
                     'product_id' => $request->product_id,
-                    'status' => $request->status,
+                    'cart_id' => $cart->id,
+                    'qty' => $old_qty
+                ]);
+                $cartData =  CartResource::collection(Cart::where('id', $cart->id)->get());
+            }else{
+                if(is_null($request->qty)){
+                    $qty =  1;
+                }else{
+                    $qty = $request->qty;
+                }
+                $new_cart = CartItem::create([
+                    'product_id' => $request->product_id,
+                    'cart_id' => $cart->id,
                     'qty' => $qty
                 ]);
-                $cart = CartResource::collection(Cart::where('id', $cart->id)->get());
+                $cartData =  CartResource::collection(Cart::where('id', $cart->id)->get());
+
             }
-            return $this->sendResponse($cart ,"Cart Added successfully!.");
+
+            // $old_cartId = Cart::where('is_active', true)->where('user_id', $user->id)->where('product_id', $request->product_id)->value('id');
+            // if($old_cartId){
+            //     $old_qty = Cart::where('is_active', true)->where('user_id', $user->id)->where('product_id', $request->product_id)->value('qty');
+            //     $new_qty = $old_qty + 1;
+            //      Cart::where('id', $old_cartId)->update([
+            //         'user_id' => $user->id,
+            //         'product_id' => $request->product_id,
+            //         'status' => $request->status,
+            //         'qty' => $new_qty
+            //     ]);
+            //     $cart = CartResource::collection(Cart::where('id', $old_cartId)->get());
+            // }else{
+            //     $qty = 1;
+            //     $cart = Cart::create([
+            //         'user_id' => $user->id,
+            //         'product_id' => $request->product_id,
+            //         'status' => $request->status,
+            //         'qty' => $qty
+            //     ]);
+            //     $cart = CartResource::collection(Cart::where('id', $cart->id)->get());
+            // }
+            return $this->sendResponse($cartData ,"Cart Added successfully!.");
         }catch(Exception $e){
             return $this->sendError($e->getMessage());
         }
